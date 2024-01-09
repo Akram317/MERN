5280325c589dca8f16c2e239a6ed881b04e33901
Akram-Zran <akram.zran@esprit.tn>
2024-01-09 13:34:59 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/')
+      .then(response => setMessage(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>MERN Stack App</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
